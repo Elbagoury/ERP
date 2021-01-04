ae04f0c7392eaa90364872c079abf7633f15df25
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-01-04 20:03:40 +0300

    [ADD] upload_progress_toast.js

---
+++ erp/addons/web_editor/static/src/components/upload_progress_toast/upload_progress_toast.js
@@ -0,0 +1,23 @@
+/** @erp-module */
+import { useService } from '@web/core/utils/hooks';
+
+import { Component, useState } from "@erp/owl";
+
+export class ProgressBar extends Component {
+    get progress() {
+        return Math.round(this.props.progress);
+    }
+}
+ProgressBar.template = 'web_editor.ProgressBar';
+
+export class UploadProgressToast extends Component {
+    setup() {
+        this.uploadService = useService('upload');
+
+        this.state = useState(this.uploadService.progressToast);
+    }
+}
+UploadProgressToast.template = 'web_editor.UploadProgressToast';
+UploadProgressToast.components = {
+    ProgressBar
+};
