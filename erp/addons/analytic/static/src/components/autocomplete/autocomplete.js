18af3beb30173773cbccf42888aec8b8f5f3fc86
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-01-30 20:18:22 +0300

    [ADD] autocomplete.js

---
+++ erp/addons/analytic/static/src/components/autocomplete/autocomplete.js
@@ -0,0 +1,14 @@
+/** @erp-module **/
+
+import { AutoComplete } from "@web/core/autocomplete/autocomplete";
+
+export class AnalyticAutoComplete extends AutoComplete {}
+AnalyticAutoComplete.template = "analytic.AutoComplete";
+AnalyticAutoComplete.props = {
+    ...AutoComplete.props,
+    onFocus: { type: Function, optional: true },
+}
+AnalyticAutoComplete.defaultProps = {
+    ...AutoComplete.defaultProps,
+    onFocus: () => {},
+}
