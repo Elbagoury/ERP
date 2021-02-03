095cc62d9375ba2578c6bbfb6728946baf9e849a
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-02-03 21:17:08 +0300

    [ADD] account_portal.js

---
+++ erp/addons/account/static/src/js/account_portal.js
@@ -0,0 +1,12 @@
+/** @erp-module */
+
+import publicWidget from 'web.public.widget';
+
+publicWidget.registry.PortalHomeCounters.include({
+    /**
+     * @override
+     */
+    _getCountersAlwaysDisplayed() {
+        return this._super(...arguments).concat(['invoice_count']);
+    },
+});
