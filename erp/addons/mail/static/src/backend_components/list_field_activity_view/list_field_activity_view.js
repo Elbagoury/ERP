3b1aa8c7a7643f31e50451c3880fcba77a28f3cf
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-02-19 18:20:48 +0300

    [ADD] list_field_activity_view.js

---
+++ erp/addons/mail/static/src/backend_components/list_field_activity_view/list_field_activity_view.js
@@ -0,0 +1,20 @@
+/** @erp-module **/
+
+import { registerMessagingComponent } from '@mail/utils/messaging_component';
+
+const { Component } = owl;
+
+export class ListFieldActivityView extends Component {
+
+    get listFieldActivityView() {
+        return this.props.record;
+    }
+
+}
+
+Object.assign(ListFieldActivityView, {
+    props: { record: Object },
+    template: 'mail.ListFieldActivityView',
+});
+
+registerMessagingComponent(ListFieldActivityView);
