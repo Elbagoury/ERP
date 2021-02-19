ffc2152a8b0b71a72e71c368c67a8225f13402f9
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-02-19 17:22:02 +0300

    [ADD] activity_list_view.js

---
+++ erp/addons/mail/static/src/backend_components/activity_list_view/activity_list_view.js
@@ -0,0 +1,20 @@
+/** @erp-module **/
+
+import { registerMessagingComponent } from '@mail/utils/messaging_component';
+
+const { Component } = owl;
+
+export class ActivityListView extends Component {
+
+    get activityListView() {
+        return this.props.record;
+    }
+
+}
+
+Object.assign(ActivityListView, {
+    props: { record: Object },
+    template: 'mail.ActivityListView',
+});
+
+registerMessagingComponent(ActivityListView);
