1f4b33a016c2166583e3e798fa4515a79257f365
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-02-21 13:56:19 +0300

    [ADD] notification_alert.js

---
+++ erp/addons/mail/static/src/components/notification_alert/notification_alert.js
@@ -0,0 +1,11 @@
+/** @erp-module **/
+
+import { registerMessagingComponent } from '@mail/utils/messaging_component';
+
+const { Component } = owl;
+
+export class NotificationAlert extends Component {}
+Object.assign(NotificationAlert, {
+    template: 'mail.NotificationAlert',
+});
+registerMessagingComponent(NotificationAlert);
