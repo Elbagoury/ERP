243140f4917e0ddb2f9f3c4f8333ef6278a9bcdb
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-02-22 19:34:17 +0300

    [ADD] notification_list.js

---
+++ erp/addons/mail/static/src/components/notification_list/notification_list.js
@@ -0,0 +1,23 @@
+/** @erp-module **/
+
+import { registerMessagingComponent } from '@mail/utils/messaging_component';
+
+const { Component } = owl;
+
+export class NotificationList extends Component {
+
+    /**
+     * @returns {NotificationListView}
+     */
+    get notificationListView() {
+        return this.props.record;
+    }
+
+}
+
+Object.assign(NotificationList, {
+    props: { record: Object },
+    template: 'mail.NotificationList',
+});
+
+registerMessagingComponent(NotificationList);
