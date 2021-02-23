e2aeb1574a597d15883237bdccd23bee811c64af
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-02-23 13:41:37 +0300

    [ADD] mail_template.js

---
+++ erp/addons/mail/static/src/components/mail_template/mail_template.js
@@ -0,0 +1,27 @@
+/** @erp-module **/
+
+import { registerMessagingComponent } from '@mail/utils/messaging_component';
+
+const { Component } = owl;
+
+export class MailTemplate extends Component {
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @returns {MailTemplateView}
+     */
+    get mailTemplateView() {
+        return this.props.record;
+    }
+
+}
+
+Object.assign(MailTemplate, {
+    props: { record: Object },
+    template: 'mail.MailTemplate',
+});
+
+registerMessagingComponent(MailTemplate);
