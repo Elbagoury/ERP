d9a7fd56a67f15fdc3f0c2c2bc19d3927b828892
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-02-24 23:00:00 +0300

    [ADD] call_settings_menu_device.js

---
+++ erp/addons/mail/static/src/components/call_settings_menu_device/call_settings_menu_device.js
@@ -0,0 +1,14 @@
+/** @erp-module **/
+
+import { registerMessagingComponent } from '@mail/utils/messaging_component';
+
+const { Component } = owl;
+
+export class CallSettingsMenuDevice extends Component {}
+
+Object.assign(CallSettingsMenuDevice, {
+    props: { device: Object },
+    template: 'mail.CallSettingsMenuDevice',
+});
+
+registerMessagingComponent(CallSettingsMenuDevice);
