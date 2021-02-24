3b9535a821b9332c67c07daa76737ef34c37c9b3
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-02-24 17:07:20 +0300

    [ADD] discuss.js

---
+++ erp/addons/mail/static/src/components/discuss/discuss.js
@@ -0,0 +1,25 @@
+/** @erp-module **/
+
+import { registerMessagingComponent } from '@mail/utils/messaging_component';
+import { LegacyComponent } from '@web/legacy/legacy_component';
+
+export class Discuss extends LegacyComponent {
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @returns {DiscussView}
+     */
+    get discussView() {
+        return this.props.record;
+    }
+}
+
+Object.assign(Discuss, {
+    props: { record: Object },
+    template: 'mail.Discuss',
+});
+
+registerMessagingComponent(Discuss);
