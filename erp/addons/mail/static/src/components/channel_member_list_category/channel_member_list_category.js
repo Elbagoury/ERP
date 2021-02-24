33b73220a14bf76fcd8128b76ce9c39b1fc48977
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-02-24 21:46:31 +0300

    [ADD] channel_member_list_category.js

---
+++ erp/addons/mail/static/src/components/channel_member_list_category/channel_member_list_category.js
@@ -0,0 +1,23 @@
+/** @erp-module **/
+
+import { registerMessagingComponent } from '@mail/utils/messaging_component';
+
+const { Component } = owl;
+
+export class ChannelMemberListCategory extends Component {
+
+    /**
+     * @returns {ChannelMemberListCategoryView}
+     */
+    get record() {
+        return this.props.record;
+    }
+
+}
+
+Object.assign(ChannelMemberListCategory, {
+    props: { record: Object },
+    template: 'mail.ChannelMemberListCategory',
+});
+
+registerMessagingComponent(ChannelMemberListCategory);
