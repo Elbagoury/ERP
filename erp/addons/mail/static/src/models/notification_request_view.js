e73dabadbe2e462be598ea500cbb668a0e4bf852
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-03-01 12:28:09 +0300

    [ADD] notification_request_view.js

---
+++ erp/addons/mail/static/src/models/notification_request_view.js
@@ -0,0 +1,34 @@
+/** @erp-module **/
+
+import { registerModel } from '@mail/model/model_core';
+import { attr, one } from '@mail/model/model_field';
+import { clear } from '@mail/model/model_field_command';
+
+import { sprintf } from '@web/core/utils/strings';
+
+registerModel({
+    name: 'NotificationRequestView',
+    fields: {
+        headerText: attr({
+            compute() {
+                if (!this.messaging.partnerRoot) {
+                    return clear();
+                }
+                return sprintf(
+                    this.env._t("%(erpbotName)s has a request"),
+                    { erpbotName: this.messaging.partnerRoot.nameOrDisplayName },
+                );
+            },
+        }),
+        notificationListViewOwner: one('NotificationListView', {
+            identifying: true,
+            inverse: 'notificationRequestView',
+        }),
+        personaImStatusIconView: one('PersonaImStatusIconView', {
+            compute() {
+                return this.messaging.partnerRoot && this.messaging.partnerRoot.isImStatusSet ? {} : clear();
+            },
+            inverse: 'notificationRequestViewOwner',
+        }),
+    },
+});
