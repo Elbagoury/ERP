fe09a2c6c7d0ce56e4a47aadac534425581289aa
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-03-23 22:15:55 +0300

    [ADD] misc.js

---
+++ erp/addons/web/static/src/search/utils/misc.js
@@ -0,0 +1,19 @@
+/** @erp-module **/
+
+export const FACET_ICONS = {
+    filter: "fa fa-filter",
+    groupBy: "oi oi-group",
+    favorite: "fa fa-star",
+    comparison: "fa fa-adjust",
+};
+
+export const GROUPABLE_TYPES = [
+    "boolean",
+    "char",
+    "date",
+    "datetime",
+    "integer",
+    "many2one",
+    "many2many",
+    "selection",
+];
