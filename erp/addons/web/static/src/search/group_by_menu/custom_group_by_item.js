dba5185aa54aad4139831f027a13e317dd31a932
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-03-24 12:42:51 +0300

    [ADD] custom_group_by_item.js

---
+++ erp/addons/web/static/src/search/group_by_menu/custom_group_by_item.js
@@ -0,0 +1,17 @@
+/** @erp-module **/
+
+import { Dropdown } from "@web/core/dropdown/dropdown";
+
+import { Component, useState } from "@erp/owl";
+
+export class CustomGroupByItem extends Component {
+    setup() {
+        this.state = useState({});
+        if (this.props.fields.length) {
+            this.state.fieldName = this.props.fields[0].name;
+        }
+    }
+}
+
+CustomGroupByItem.template = "web.CustomGroupByItem";
+CustomGroupByItem.components = { Dropdown };
