4b5a9a4cc7549fd25fb554a32ca9f4a03bfcda6b
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-03-28 15:09:47 +0300

    [ADD] legacy_promise_error_handler.js

---
+++ erp/addons/web/static/src/legacy/legacy_promise_error_handler.js
@@ -0,0 +1,38 @@
+/** @erp-module **/
+
+import { registry } from "@web/core/registry";
+
+/**
+ * @typedef {import("../env").ERPEnv} ERPEnv
+ * @typedef {import("../core/errors/error_service").UncaughtPromiseError} UncaughtPromiseError
+ */
+
+// -----------------------------------------------------------------------------
+// Legacy Promise error handling
+// -----------------------------------------------------------------------------
+
+/**
+ * @param {ERPEnv} env
+ * @param {Error} error
+ * @param {Error} originalError
+ * @returns {boolean}
+ */
+function legacyRejectPromiseHandler(env, error, originalError) {
+    if (error.name.startsWith("UncaughtPromiseError")) {
+        const isLegitError = originalError && originalError instanceof Error;
+        const isLegacyRPC = originalError && originalError.legacy;
+        if (!isLegitError && !isLegacyRPC) {
+            // we consider that a code throwing something that is not an error is
+            // a case where it is meant as an asynchronous control flow (as legacy
+            // code is sadly doing). For now, we just want to consider this as a non
+            // error, so we prevent default it.
+            error.unhandledRejectionEvent.preventDefault();
+            return true;
+        }
+    }
+    return false;
+}
+
+registry
+    .category("error_handlers")
+    .add("legacyRejectPromiseHandler", legacyRejectPromiseHandler, { sequence: 1 });
