1633eee29c9a3bb18259c9a973630dcc23c1d8d4
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-04-05 19:48:58 +0300

    [ADD] handle_field.js

---
+++ erp/addons/web/static/src/views/fields/handle/handle_field.js
@@ -0,0 +1,19 @@
+/** @erp-module **/
+
+import { registry } from "@web/core/registry";
+import { _lt } from "@web/core/l10n/translation";
+import { standardFieldProps } from "../standard_field_props";
+
+import { Component } from "@erp/owl";
+
+export class HandleField extends Component {}
+
+HandleField.template = "web.HandleField";
+HandleField.props = {
+    ...standardFieldProps,
+};
+HandleField.displayName = _lt("Handle");
+HandleField.supportedTypes = ["integer"];
+HandleField.isEmpty = () => false;
+
+registry.category("fields").add("handle", HandleField);
