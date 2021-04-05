9c51f58180585c1a0da768617df4eb0d03ae0636
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-04-05 12:13:28 +0300

    [ADD] list_x2many_field.js

---
+++ erp/addons/web/static/src/views/fields/x2many/list_x2many_field.js
@@ -0,0 +1,21 @@
+/** @erp-module **/
+
+import { registry } from "@web/core/registry";
+import { formatX2many } from "../formatters";
+import { standardFieldProps } from "../standard_field_props";
+
+import { Component } from "@erp/owl";
+
+export class ListX2ManyField extends Component {
+    get formattedValue() {
+        return formatX2many(this.props.value);
+    }
+}
+
+ListX2ManyField.template = "web.ListX2ManyField";
+ListX2ManyField.props = { ...standardFieldProps };
+
+ListX2ManyField.useSubView = false;
+
+registry.category("fields").add("list.one2many", ListX2ManyField);
+registry.category("fields").add("list.many2many", ListX2ManyField);
