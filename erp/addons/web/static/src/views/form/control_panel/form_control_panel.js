535a03bdac9042ec7f1fc42185de5440cd105fb5
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-04-08 13:26:56 +0300

    [ADD] form_control_panel.js

---
+++ erp/addons/web/static/src/views/form/control_panel/form_control_panel.js
@@ -0,0 +1,6 @@
+/** @erp-module **/
+
+import { ControlPanel } from "@web/search/control_panel/control_panel";
+
+export class FormControlPanel extends ControlPanel {}
+FormControlPanel.template = "web.FormControlPanel";
