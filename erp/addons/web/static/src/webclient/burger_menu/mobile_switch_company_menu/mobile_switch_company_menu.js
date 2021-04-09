3cafff33bf1231d0c32f7dda9f11a314a52e87b3
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2021-04-09 14:55:06 +0300

    [ADD] mobile_switch_company_menu.js

---
+++ erp/addons/web/static/src/webclient/burger_menu/mobile_switch_company_menu/mobile_switch_company_menu.js
@@ -0,0 +1,5 @@
+/** @erp-module **/
+import { SwitchCompanyMenu } from "@web/webclient/switch_company_menu/switch_company_menu";
+
+export class MobileSwitchCompanyMenu extends SwitchCompanyMenu {}
+MobileSwitchCompanyMenu.template = "web.MobileSwitchCompanyMenu";
